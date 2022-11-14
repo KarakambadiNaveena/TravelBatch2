f0611e96244dc8b9251978401460ce4b105f0a9e
KarakambadiNaveena <naveenakarakambadi@gmail.com>
2022-11-14 20:16:20 +0530

    upload

---
+++ travelapp1/Controllers/AdminController.cs
@@ -0,0 +1,57 @@
+﻿using DAL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace travelapp1.Controllers
+{
+    public class AdminController : Controller
+    {
+        Operations cd = new Operations();
+        // GET: Admin
+        public ActionResult Index()
+        {
+           
+                List<User> l = new List<User>();
+                l = cd.GetUser();
+                return View(l);
+            }
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection c)
+        {
+            string s = c["AdminEmail"].ToString();
+            string k = c["AdminPassword"].ToString();
+            bool k1 = false;
+            foreach (var item in cd.GetAdmin())
+            {
+                if (item.AdminEmail == s && item.AdminPassword == k)
+                {
+                    TempData["User"] = item;
+                    Session["u1"] = item;
+                    k1 = true;
+                }
+            }
+            if (k1)
+            {
+                
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Credentials..Try Again";
+                return View();
+            }
+
+
+        }
+    }
+}
---
+++ travelapp1/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace travelapp1.Controllers
+{
+    public class UserController : Controller
+    {
+        Operations cd = new Operations();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UserLogin(FormCollection c)
+        {
+            string s = c["Email"].ToString();
+            string k = c["Password"].ToString();
+            bool k1 = false;
+            foreach (var item in cd.GetUser())
+            {
+                if (item.Email == s && item.Password == k)
+                {
+                    TempData["User"] = item;
+                    Session["u1"] = item;
+                    k1 = true;
+                }
+            }
+            if (k1)
+            {
+                return RedirectToAction("Index");
+               
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Credentials..Try Again";
+                return View();
+            }
+
+
+        }
+    }
+}
